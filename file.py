1b63895c31e835dc77b6413af94933373717555b
student <studetn2014>
2019-07-01 14:21:34 -0700

    Created 2 Python Files; TRPG Test And Started Classwork

---
+++ file.py
@@ -0,0 +1,8 @@
+import random
+for x in range(1):
+  print( random.randint(1,101) )
+
+print("What Class Would You Like To Chose?")
+Class = input("1. Knight    2. Rogue    3. Mage")
+
+
--- main.py
+++ main.py
@@ -3,3 +3,17 @@ def main():
 
 if __name__ == "__main__":
         main()
+
+        name = input("What is your name?")
+        print("Hello %s" %name)
+
+        num = int(input("Enter a Number"))
+
+        if num > 10:
+            print("The Number Is Greater Than 10")
+        if num < 10:
+            print("The Number Is Less Than 10")
+        if num == 10:
+            print("The Number Is 10")
+        else:
+            print("That Is Not A Number")
