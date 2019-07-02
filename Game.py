48a39f91d9ae2dbf678e9e39ee14a2e70985c332
student <studetn2014>
2019-07-02 15:56:50 -0700

    Update; Started Side Game

--- Game.py
+++ Game.py
@@ -10,9 +10,8 @@ screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 color = (255, 0, 0)
 
-
 sprite_image = pygame.image.load("porpoise.png")
-sprite_image = pygame.transform.smoothscale(sprite_image, (280, 280))
+sprite_image = pygame.transform.smoothscale(sprite_image, (80, 80))
 sprite_rect = sprite_image.get_rect()
 
 sprite_rect.center = (width // 2, height // 2)
@@ -24,18 +23,27 @@ sprite_image = pygame.transform.rotate(sprite_image, 180 - rotation)
 
 
 def move_sprite():
-    sprite_rect.move.ip(speed)
+    global sprite_image
+    screen_info = pygame.display.Info()
+    sprite_rect.move_ip(speed)
+    if sprite_rect.left < 0 or sprite_rect.right > screen_info.current_w:
+        speed[0] *= -1
+        sprite_image = pygame.transform.flip(sprite_image, True, False)
+        sprite_rect.move_ip(speed[0], 0)
+    if sprite_rect.top < 0 or sprite_rect.bottom > screen_info.current_h:
+        speed[1] *= -1
+        sprite_image = pygame.transform.flip(sprite_image, False, True)
+        sprite_rect.move_ip(0, speed[1])
 
 
 def main():
     while True:
         clock.tick(60)
+        move_sprite()
         screen.fill(color)
         screen.blit(sprite_image, sprite_rect)
         pygame.display.flip()
 
 
-
 if __name__ == '__main__':
     main()
-
